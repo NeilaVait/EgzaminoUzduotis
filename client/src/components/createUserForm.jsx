2308f62b4e15f03036d8b2cf6028ff103e5783fa
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-28 09:49:19 +0300

    inputs binded, handlesubmit prepares data to send, clear inputs fn

--- client/src/components/createUserForm.jsx
+++ client/src/components/createUserForm.jsx
@@ -1,22 +1,93 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component {
-  state = {};
+  constructor(props) {
+    super();
+    this.state = {
+      formData: {
+        username: '',
+        age: '',
+        email: '',
+        password: '',
+      },
+    };
+  }
+
+  clearInputs = () => {
+    const clearedFormData = {
+      username: '',
+      age: '',
+      email: '',
+      password: '',
+    };
+
+    this.setState({ formData: clearedFormData });
+  };
+
+  handleLocalSubmit = (e) => {
+    e.preventDefault();
+    console.log('click');
+    const { username, age, email, password } = this.state.formData;
+    const dataToSend = {
+      username,
+      age,
+      email,
+      password,
+    };
+    console.log('datatosend', dataToSend);
+    this.clearInputs();
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ formData: { ...this.state.formData, [name]: value } });
+  };
+
   render() {
+    const { formData: f } = this.state;
+
     return (
       <div className="user-form-container w-50">
-        <form>
+        <form autoComplete="off" onSubmit={this.handleLocalSubmit}>
           <div className="form-group">
-            <input name="username" type="text" className="form-control" placeholder="Vartotojo vardas" />
+            <input
+              value={f.username}
+              onChange={this.handleChange}
+              name="username"
+              type="text"
+              className="form-control"
+              placeholder="Vartotojo vardas"
+            />
           </div>
           <div className="form-group">
-            <input name="age" type="number" className="form-control" placeholder="Amžius" />
+            <input
+              value={f.age}
+              onChange={this.handleChange}
+              name="age"
+              type="number"
+              className="form-control"
+              placeholder="Amžius"
+            />
           </div>
           <div className="form-group">
-            <input name="email" type="email" className="form-control" placeholder="El. pašto adresas" />
+            <input
+              value={f.email}
+              onChange={this.handleChange}
+              name="email"
+              type="email"
+              className="form-control"
+              placeholder="El. pašto adresas"
+            />
           </div>
           <div className="form-group">
-            <input name="password" type="password" className="form-control" placeholder="Slaptažodis" />
+            <input
+              value={f.password}
+              onChange={this.handleChange}
+              name="password"
+              type="password"
+              className="form-control"
+              placeholder="Slaptažodis"
+            />
           </div>
 
           <button type="submit" className="btn btn-primary">
