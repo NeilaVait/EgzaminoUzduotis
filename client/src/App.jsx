8c14a1c04a6c1760e1a5975774f83090bb66260a
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-28 09:20:44 +0300

    bootstrap installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class App extends Component {
   state = {};
   render() {
-    return <div className="App"></div>;
+    return <div className="App">app</div>;
   }
 }
 
