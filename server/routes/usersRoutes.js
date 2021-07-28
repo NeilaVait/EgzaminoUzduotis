d7937f8c90a37b5540e5eac00d0980b06fa37aca
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-28 09:57:49 +0300

    add user route

---
+++ server/routes/usersRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const User = require('../model/userModel');
+const router = express.Router();
+
+// add new user
+router.post('/api/users/new', async (req, res) => {
+  const newUserData = req.body;
+  console.log(newUserData);
+  const newUser = new User(req.body);
+
+  try {
+    const newUserResult = await newUser.save();
+    res.json({ success: true });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
